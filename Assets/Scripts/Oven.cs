678a30fb0d66e5afc0695b6d884e21ae3cf853f6
Gianlucca <agianlucca@gmail.com>
2024-02-17 13:02:53 +0100

    pizza Oven Interaction

--- Assets/Scripts/Oven.cs
+++ Assets/Scripts/Oven.cs
@@ -2,32 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PizzaPeel : MonoBehaviour
+public class Oven : MonoBehaviour
 {
     public Transform GrabPosition;
-    public bool isGrabbed;
+    public bool isCooking;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        
     }
 
     public void OnTriggerEnter(Collider other)
     {
-        Debug.Log("Peel Collided with " + other.gameObject.name);
+        Debug.Log("Oven Collided with " + other.gameObject.name);
 
         if (other.gameObject.name == "Pizza")
         {
-            isGrabbed = true;
-            other.gameObject.transform.position = GrabPosition.position;
+            isCooking = true;
 
+            other.gameObject.transform.position = GrabPosition.position;
             other.gameObject.transform.parent = GrabPosition;
         }
     }
