31a92213848613a31ab058cdca5c1ff771fd5b42
Gianlucca <agianlucca@gmail.com>
2024-02-17 12:10:16 +0100

    add pina

--- Assets/Scripts/PizzaPeel.cs
+++ Assets/Scripts/PizzaPeel.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class PizzaPeel : MonoBehaviour
 {
+    public Transform GrabPosition;
+    public bool isGrabbed;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Collided with " + other.gameObject.name);
+
+        if (other.gameObject.name == "Pizza")
+        {
+            isGrabbed = true;
+            other.gameObject.transform.position = GrabPosition.position;
+
+            other.gameObject.transform.parent = GrabPosition;
+            other.gameObject.GetComponent<Rigidbody>().isKinematic = true;
+        }
     }
 }
